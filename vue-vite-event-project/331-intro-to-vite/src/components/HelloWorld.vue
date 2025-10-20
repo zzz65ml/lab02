0093c5efdc114bb7b34aa5ca43d45a586a229b0b
20232089 <2804973446@qq.com>
2025-10-20 23:00:51 +0800

    1

---
+++ vue-vite-event-project/331-intro-to-vite/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// 注释原有Props定义，不再接收msg参数
+// defineProps<{
+//   msg: string
+// }>()
+</script>
+
+<template>
+  <!-- 清空原有内容，保留空容器 -->
+  <div class="greetings"></div>
+</template>
+
+<!-- 删除原有样式，保留空style标签 -->
+<style scoped></style>
